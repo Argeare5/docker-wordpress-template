616675411d098a190cfd9f717dbb88ed041b1d91
Alex <argeares97@gmail.com>
2025-05-15 12:25:00 +0300

    fix

---
+++ wp-content/plugins/social-sharing/social-sharing.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Social Sharing
+ * Plugin URI: https://yourwebsite.com
+ * Description: A simple plugin to add social sharing buttons to your posts.
+ * Version: 1.0
+ * Author: Your Name
+ * Author URI: https://yourwebsite.com
+ * License: GPL2
+ */
+?>
